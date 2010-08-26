f316710b7e8e1082ad38bb7d1a1edfc3de62456c
johnrobertpanzer <johnrobertpanzer@0c3e17fe-a321-11de-97f0-97815e7b2756>
2010-08-26 17:04:17 +0000

    An experimental draft of an updated Magic Signatures specification, intended to become an -01 version if accepted.  In addition to various clarifications, it adds more support for extensibility, adds minimal support for HMAC signatures in core, allows for arbitrary key ids, and provides for an alternative direct public key retrieval via signer URI mechanism.  This draft is published for discussion.

---
+++ makedocs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/Applications/xml2rfc-1.35/xml2rfc.tcl draft-panzer-magicsig-experimental-00.xml draft-panzer-magicsig-experimental-00.html && open draft-panzer-magicsig-experimental-00.html
