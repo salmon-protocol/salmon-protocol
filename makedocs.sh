619e98cd761af7409dd8111747a423ab5c4c480b
johnrobertpanzer <johnrobertpanzer@0c3e17fe-a321-11de-97f0-97815e7b2756>
2011-01-08 07:03:08 +0000

    1. Moving Magic Signatures experimental draft to new -01 mainline, adjusting references. 2. Final edits & acknowledgements sections for both documents. 3. Moving the "provenance" specification text from Magic Signatures to Salmon (as it is fairly Salmon specific) 4. Fixing the base64url padding issue to reflect consensus & align w/JWT & other usage.
    Only known open issue is double checking all examples & likely updating based on final code cross-checked 
    across multiple libraries.

--- makedocs.sh
+++ makedocs.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-/Applications/xml2rfc-1.35/xml2rfc.tcl draft-panzer-magicsig-experimental-00.xml draft-panzer-magicsig-experimental-00.html && open draft-panzer-magicsig-experimental-00.html
+~/tools/xml2rfc-1.35/xml2rfc.tcl draft-panzer-magicsig-01.xml draft-panzer-magicsig-01.html && open draft-panzer-magicsig-01.html 
+~/tools/xml2rfc-1.35/xml2rfc.tcl draft-panzer-salmon-00.xml draft-panzer-salmon-00.html && open draft-panzer-salmon-00.html
+
